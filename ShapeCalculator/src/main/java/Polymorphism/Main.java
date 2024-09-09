a61f96da0fe3b9290dbc2c0f989cd6d3f4dd5c99
prairie <prg@cs.unc.edu>
2024-09-09 12:39:44 -0400

    beginning state of lecture 6

---
+++ ShapeCalculator/src/main/java/Polymorphism/Main.java
@@ -0,0 +1,91 @@
+package Polymorphism;
+
+import People.*;
+
+public class Main {
+    public static void main(String[] args){
+        Professor prairie1 = new Professor("prairie");
+        Person prairie2 =  prairie1;
+
+    }
+
+    public static String example1(){
+        String retval = "";
+
+        Person jane = new Person("Jane");
+
+
+        if (jane instanceof Professor) {
+            retval += "foo";
+        }
+
+
+        if (jane instanceof Student) {
+            retval += "bar";
+        }
+
+
+        if (jane instanceof Person) {
+            retval += "!";
+        }
+        return retval;
+    }
+    
+    public static String pollEv1(){
+        String retval = "";
+        Student hanna = new Student("hanna");
+
+
+        if (hanna instanceof Student) {
+            retval += "foo";
+        }
+
+
+        if (hanna instanceof Person) {
+            retval += "bar";
+        }
+
+        return retval;
+    }
+
+    public static String pollEv2(){
+        String retval = "";
+        Person prg = new Professor("prg");
+
+
+        if (prg instanceof Professor) {
+            retval += "foo";
+        }
+
+
+        if (prg instanceof Student) {
+            retval += "bar";
+        }
+
+
+        if (prg instanceof Person) {
+            retval += "!";
+        }
+        return retval;
+    }
+
+    public static void pollEv3(){
+
+        Student hanna = new Student("hanna");
+        Person prairie = new Professor("Prairie");
+
+        Person hanna2 = (Person) hanna;// 1)
+
+
+        Professor prairie2 = (Professor) prairie;// 2)
+
+
+        Student prairie3 = (Student) prairie;// 3)
+
+
+    }
+
+
+
+
+}
---
+++ ShapeCalculator/src/test/java/Polymorphism/PolymorphismTest.java
@@ -0,0 +1,14 @@
+package Polymorphism;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PolymorphismTest {
+
+
+    @Test
+    void test_example1() {
+    }
+
+}
