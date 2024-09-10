61371776cd67c8b6fa2f02729254e875be19b82f
prairie <prg@cs.unc.edu>
2024-09-10 14:02:09 -0400

    beginning state of lecture 7

---
+++ ShapeCalculator/src/main/java/AsciiLab/Main.java
@@ -0,0 +1,7 @@
+package AsciiLab;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("(^_^.)");
+    }
+}
---
+++ ShapeCalculator/src/test/java/AsciiLab/AsciiTest.java
@@ -0,0 +1,7 @@
+package AsciiLab;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsciiTest {
+
+}
